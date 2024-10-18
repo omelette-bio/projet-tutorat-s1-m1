cd71f660ebbaf32a70376fcd6c4ff298c3ba81da
omelettebio <omelettebio@proton.me>
2024-10-18 15:51:39 +0200

    premieres data de compil

--- time_exec_script.sh
+++ time_exec_script.sh
@@ -12,7 +12,11 @@ do
         do
             for i in {1..12}; 
             do
-                /usr/bin/time -f "%e" ./$prog-$comp$opt 2>> time_data.txt > /dev/null
+                echo "executing $prog-$comp$opt"
+                #/usr/bin/time -f "%e" ./$prog-$comp$opt 2>> time_data.txt > /dev/null
+                /usr/bin/time -f %e ./$prog-$comp$opt > /dev/null 2> zob.txt
+                exec_time=$(cat zob.txt)
+                echo "$comp$opt : $exec_time" >> time_data.txt
             done;
         done
     done
